bc4ecaca765929a658b0bdcfba3d92884434f5e6
crydestaahl <kristoffer@oholics.com>
2019-03-25 14:26:30 +0100

    Updates

---
+++ precache-manifest.8ddfbe51cb6e19fd4059a67447e21adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a5977e9a311e34fc37878fc21a3adaa",
+    "url": "/resume/static/media/jag.1a5977e9.png"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "a363d9762f01e158ea8e",
+    "url": "/resume/static/js/main.f7e0101c.chunk.js"
+  },
+  {
+    "revision": "099f4f5bae91a2410af1",
+    "url": "/resume/static/js/2.2435743b.chunk.js"
+  },
+  {
+    "revision": "a363d9762f01e158ea8e",
+    "url": "/resume/static/css/main.2f962817.chunk.css"
+  },
+  {
+    "revision": "260657d31bb04f96b60e215c7261cb3a",
+    "url": "/resume/index.html"
+  }
+];
